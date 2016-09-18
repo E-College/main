1b48d20fa450bc9e6301a918a4da514c884461b8
harshit soni <harshit soni>
2016-09-18 23:07:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/main/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.hp.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void topHundredColleges(){
+
+
+    }
+    public void collegeSearch(){
+
+    }
+    public void entranceExams(){
+
+    }
+    public void topStateColleges(){
+
+    }
+    public void eligibility(){
+
+    }
+    public void scholarshipTest(){
+
+    }
+}
